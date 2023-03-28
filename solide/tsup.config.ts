971206507943c8dece5a8b46cf017637d2f4884b
Joël Charles <joel.charles91@gmail.com>
2023-03-28 17:26:24 +0200

    trying tsup-preset-solid

--- solide/tsup.config.ts
+++ solide/tsup.config.ts
@@ -1,19 +1,38 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsup-preset-solid";
 
-export default defineConfig({
-  entry: [
-    "./renderer/+onRenderHtml.tsx",
-    "./renderer/+onRenderClient.tsx",
-    "./renderer/+config.ts",
-    "./renderer/+passToClient.ts",
-    "./renderer/+onPageTransitionStart.ts",
-    "./renderer/+onPageTransitionEnd.ts",
-    "./components/usePageContext.tsx",
-    "./cli/index.ts",
-    "./index.ts",
+export default defineConfig(
+  [
+    {
+      entry: "./renderer/+onRenderHtml.tsx",
+    },
+    {
+      entry: "./renderer/+onRenderClient.tsx",
+    },
+    {
+      entry: "./renderer/+config.ts",
+    },
+    {
+      entry: "./renderer/+passToClient.ts",
+    },
+    {
+      entry: "./renderer/+onPageTransitionStart.ts",
+    },
+    {
+      entry: "./renderer/+onPageTransitionEnd.ts",
+    },
+    {
+      entry: "./components/usePageContext.tsx",
+    },
+    {
+      entry: "./cli/index.ts",
+      devEntry: true,
+    },
+    {
+      entry: "./index.ts",
+    },
   ],
-  format: "esm",
-  clean: true,
-  // sourcemap: true,
-  dts: true,
-});
+  {
+    // Setting `true` will console.log the package.json fields
+    // printInstructions: true,
+  }
+);
