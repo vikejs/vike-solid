f92c2d78091a6cd01bd9db160318dc747471e1f2
Romuald Brillout <git@brillout.com>
2023-03-31 16:25:44 +0200

    update vps

--- solide/renderer/getTitle.ts
+++ solide/renderer/getTitle.ts
@@ -1,11 +1,12 @@
 export { getTitle };
 
 import { isCallable } from "./utils/isCallable";
+import type { ConfigEntries } from "vite-plugin-ssr/types";
 
 function getTitle(pageContext: {
   title?: unknown;
   config: Record<string, unknown>;
-  configList: Record<string, undefined | { configOrigin: string }[]>;
+  configEntries: ConfigEntries;
 }): null | string {
   if (typeof pageContext.title === "string") {
     return pageContext.title;
@@ -20,18 +21,17 @@ function getTitle(pageContext: {
   if (!title) {
     return null;
   }
-  const { configOrigin } = pageContext.configList.title![0]!;
+  const { configDefinedAt } = pageContext.configEntries.title![0]!;
   if (isCallable(title)) {
     const val = title(pageContext);
     if (typeof val === "string") {
       return val;
     }
     if (val) {
-      throw new Error(configOrigin + " `export { title }` should return a string");
+      throw new Error(configDefinedAt + " should return a string");
     }
   }
   throw new Error(
-    configOrigin +
-      " `export { title }` should be a string or a function returning a string"
+    configDefinedAt + " should be a string or a function returning a string"
   );
 }
