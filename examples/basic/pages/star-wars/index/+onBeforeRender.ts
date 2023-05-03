1935ce7a93f18ee8c0963938ac21f92f6e7c0dc5
Romuald Brillout <git@brillout.com>
2023-05-03 21:18:11 +0200

    minor

--- examples/basic/pages/star-wars/index/+onBeforeRender.ts
+++ examples/basic/pages/star-wars/index/+onBeforeRender.ts
@@ -51,7 +51,7 @@ async function prerender() {
         pageProps: {
           movies: filterMoviesData(movies)
         },
-        documentProps: { title: getTitle(movies) }
+        title: getTitle(movies)
       }
     },
     ...movies.map((movie) => {
@@ -66,7 +66,7 @@ async function prerender() {
           pageProps: {
             movie: filterMovieData(movie)
           },
-          documentProps: { title: movie.title }
+          title: movie.title
         }
       }
     })
