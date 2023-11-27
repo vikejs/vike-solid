3511fe9311585bc864452fe848f7365e0e753d6f
Joël Charles <joel.charles91@gmail.com>
2023-11-27 11:03:59 +0100

    feat: ClientOnly component

---
+++ vike-solid/components/ClientOnly.tsx
@@ -0,0 +1,36 @@
+import type { Component, JSX } from "solid-js";
+import { createEffect, createSignal, lazy, Suspense } from "solid-js";
+import { Dynamic } from "solid-js/web";
+
+function ClientOnlyError() {
+  return <p>Error loading component.</p>
+}
+
+export function ClientOnly<T>(props: {
+  load: () => Promise<{ default: Component<T> } | Component<T>>
+  children: (Component: Component<T>) => JSX.Element
+  fallback: JSX.Element
+  deps?: Parameters<typeof createSignal>[1]
+}) {
+  const [getComponent, setComponent] = createSignal<Component<unknown> | undefined>(undefined);
+
+  createEffect(() => {
+    const loadComponent = () => {
+      const Component = lazy(() =>
+        props.load()
+          .then((LoadedComponent) => {
+            return { default: () => props.children("default" in LoadedComponent ? LoadedComponent.default : LoadedComponent) };
+          })
+          .catch((error) => {
+            console.error("Component loading failed:", error);
+            return { default: ClientOnlyError };
+          })
+      );
+      setComponent(() => Component);
+    };
+
+    loadComponent();
+  });
+
+  return <Suspense fallback={props.fallback}><Dynamic component={getComponent()} /></Suspense>;
+}
