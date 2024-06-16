e6a38efa425de8adb6873b0f9d28e24f91cc79e7
Blankeos <carloantonioct@gmail.com>
2024-06-16 23:47:06 +0800

    add nested layout example

--- examples/basic/pages/+config.ts
+++ examples/basic/pages/+config.ts
@@ -1,5 +1,5 @@
-import type { Config } from "vike/types";
 import vikeSolid from "vike-solid/config";
+import type { Config } from "vike/types";
 
 // Default config (can be overridden by pages)
 export default {
