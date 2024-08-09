637a87fdf79d8294f4158e1e66631ff167cff423
Romuald Brillout <git@brillout.com>
2024-08-09 11:19:13 +0200

    simplify Head-client.ts

--- vike-solid/components/Head/Head-client.ts
+++ vike-solid/components/Head/Head-client.ts
@@ -1,14 +1,3 @@
-export { Head };
-
-// Same as ./Head-server.ts but importing useConfig-client.js
-
-/**
- * Add arbitrary `<head>` tags.
- *
- * (The children are teleported to `<head>`.)
- *
- * https://vike.dev/Head
- */
-function Head(): null {
+export function Head(): null {
   return null;
 }
