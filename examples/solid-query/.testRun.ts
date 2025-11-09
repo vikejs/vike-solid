85b2aadbba60c53a54202f190175e221049e41d2
Romuald Brillout <git@brillout.com>
2025-11-09 15:06:15 +0100

    update test

--- examples/solid-query/.testRun.ts
+++ examples/solid-query/.testRun.ts
@@ -1,7 +1,7 @@
 export { testRun };
 
 import { autoRetry, expect, getServerUrl, page, partRegex, run, test } from "@brillout/test-e2e";
-const dataHk = partRegex`data-hk=${/[0-9-]+/}`;
+const dataHk = partRegex`data-hk="${/[0-9-]+/}"`;
 
 function testRun(cmd: `pnpm run ${"dev" | "preview"}`) {
   run(cmd, {
