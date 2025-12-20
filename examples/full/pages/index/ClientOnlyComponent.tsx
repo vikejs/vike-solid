556570b8224698d5b3b4d4430a2f50f6c462762b
Romuald Brillout <git@brillout.com>
2025-12-20 14:02:54 +0100

    update to-do

--- examples/full/pages/index/ClientOnlyComponent.tsx
+++ examples/full/pages/index/ClientOnlyComponent.tsx
@@ -1,3 +1,5 @@
+// TODO/ai change it to https://github.com/vikejs/vike-react/blob/8224b4dbf34e6d7c718243ef24063056f3e59a29/examples/full/components/ClientOnlyComponent.tsx#L3 and update test
+
 console.log('This will only run on the client side.')
 
 export default function ClientOnlyComponent() {
