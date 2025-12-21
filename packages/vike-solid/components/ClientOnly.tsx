87ad30c9de83048742fdb299bdaadcdd65a9f817
Romuald Brillout <git@brillout.com>
2025-12-21 21:25:18 +0100

    update to-do

--- packages/vike-solid/components/ClientOnly.tsx
+++ packages/vike-solid/components/ClientOnly.tsx
@@ -28,3 +28,24 @@ export function ClientOnly(props: { children?: JSX.Element; fallback?: JSX.Eleme
     </Show>
   );
 }
+
+/**
+ * Return a boolean indicating if the JS has been hydrated already.
+ * When doing Server-Side Rendering, the result will always be false.
+ * When doing Client-Side Rendering, the result will always be false on the
+ * first render and true from then on. Even if a new component renders it will
+ * always start with true.
+ *
+ * Example: Disable a button that needs JS to work.
+ * ```tsx
+ * let hydrated = useHydrated();
+ * return (
+ *   <button type="button" disabled={!hydrated} onClick={doSomethingCustom}>
+ *     Click me
+ *   </button>
+ * );
+ * ```
+ */
+function useHydrated() {
+  // TODO/ai implement using pageContext.isClientSide and pageContext.isHydration
+}
