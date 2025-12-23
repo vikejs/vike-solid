9d6af087a8e3284a68e3cb3e16bf544584dcf57c
Romuald Brillout <git@brillout.com>
2025-12-23 11:14:18 +0100

    fix: furhter avoid vite type mismatch

--- packages/vike-solid/vite.config.ts
+++ packages/vike-solid/vite.config.ts
@@ -3,12 +3,14 @@ import type { UserConfig } from "vite";
 
 const root = process.cwd();
 
-const config: UserConfig = {
+// Return `UserConfigInterop` instead of `UserConfig` to avoid type mismatch upon different Vite versions
+type UserConfigInterop = Record<string, unknown>;
+const config: UserConfigInterop = {
   root,
   plugins: [vpvs()],
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : undefined,
   },
-};
+} satisfies UserConfig;
 
 export default config;
